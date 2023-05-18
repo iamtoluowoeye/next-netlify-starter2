afd560f83959d0f8a47180489fb71ed54e0fa6f1
iamtoluowoeye <134011945+iamtoluowoeye@users.noreply.github.com>
2023-05-18 19:16:32 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,44 @@
+<!DOCTYPE html>
+<html>
 <head>
   <meta charset="UTF-8">
-  <meta property="og:image" content="http://www.woman-themovie.org/wp-content/uploads/2020/02/CHAUD_Celine-W_78-FRANCE_24-FACIES-LVMH-CALLIGARO_Sandra-V.jpg" />
+  <meta property="og:image" content="http://www.woman-themovie.org/wp-content/uploads/2020/02/CHAUD_Celine-W_78-FRANCE_24-FACIES-LVMH-CALLIGARO_Sandra-V.jpg">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Your Title Here</title>
 </head>
+<body>
 <center>
   <h1>I-HD Chofoza Lapha Ukuze Uyibuke👇</h1>
-    <br />
-<a target="_blank" href="https://www.highrevenuecpmnetwork.com/hwpbg1a0?key=c6a03f0489a934cfed12612f2d02c4fa"><img src="https://stupendous-maz.netlify.app/Untitled%20design%20(5).png" alt="Five cats looking around a field." width="350" height="300"></a>
-<br />
-  
- 
-<a target="_blank" href="https://www.highrevenuegate.com/k2teiyuizm?key=89e88bacc97dd96ca05f751701656cdf" ><img src="https://stupendous-maz.netlify.app/play.gif" alt="Five cats looking around a field." width="320" height="100"></a>
-<br />
   <br />
+  <a target="_blank" href="https://www.highrevenuecpmnetwork.com/hwpbg1a0?key=c6a03f0489a934cfed12612f2d02c4fa">
+    <img src="https://stupendous-maz.netlify.app/Untitled%20design%20(5).png" alt="Five cats looking around a field." width="350" height="300">
+  </a>
   <br />
-  <br />
-    <a target="_blank" href="https://www.highrevenuegate.com/k2teiyuizm?key=89e88bacc97dd96ca05f751701656cdf" ><img src="https://stupendous-maz.netlify.app/whats.gif" alt="Five cats looking around a field." width="350" height="100"></a>
-<h2>khuluma nami kuwhatsapp</h2>
-  <br />
-  <br />
-  <br />
-<a target="_blank" href="https://www.highrevenuegate.com/k2teiyuizm?key=89e88bacc97dd96ca05f751701656cdf" ><img src="https://asepagri.com/wp-content/uploads/2023/02/Screenshot_20230206-042453-300x150.png" alt="Five cats looking around a field." width="320" height="100"></a>
-<br />
-  <br />
-  <br />
-  <a target="_blank" href="https://www.highrevenuegate.com/k2teiyuizm?key=89e88bacc97dd96ca05f751701656cdf" ><img src="https://stupendous-maz.netlify.app/newww.gif" alt="Five cats looking around a field." width="320" height="100"></a>
-<br />
-  <br />
-  <br />
-  <br />
-
- <a target="_blank" href="https://www.highrevenuegate.com/k2teiyuizm?key=89e88bacc97dd96ca05f751701656cdf" ><img src="https://stupendous-maz.netlify.app/faceboo.gif" alt="Five cats looking around a field." width="320" height="100"></a>
-<h2>ngibuyele ku-facebook</h2>
-  <br />
-  <br />
-  <br />
-  <a target="_blank" href="https://www.highrevenuegate.com/k2teiyuizm?key=89e88bacc97dd96ca05f751701656cdf" ><img src="https://stupendous-maz.netlify.app/pl%20(2).jpg" alt="Five cats looking around a field." width="350" height="100"></a>
-
+  <a target="_blank" href="https://www.highrevenuegate.com/k2teiyuizm?key=89e88bacc97dd96ca05f751701656cdf">
+    <img src="https://stupendous-maz.netlify.app/play.gif" alt="Five cats looking around a field." width="320" height="100">
+  </a>
+  <br /><br /><br />
+  <a target="_blank" href="https://www.highrevenuegate.com/k2teiyuizm?key=89e88bacc97dd96ca05f751701656cdf">
+    <img src="https://stupendous-maz.netlify.app/whats.gif" alt="Five cats looking around a field." width="350" height="100">
+  </a>
+  <h2>khuluma nami kuwhatsapp</h2>
+  <br /><br /><br />
+  <a target="_blank" href="https://www.highrevenuegate.com/k2teiyuizm?key=89e88bacc97dd96ca05f751701656cdf">
+    <img src="https://asepagri.com/wp-content/uploads/2023/02/Screenshot_20230206-042453-300x150.png" alt="Five cats looking around a field." width="320" height="100">
+  </a>
+  <br /><br /><br />
+  <a target="_blank" href="https://www.highrevenuegate.com/k2teiyuizm?key=89e88bacc97dd96ca05f751701656cdf">
+    <img src="https://stupendous-maz.netlify.app/newww.gif" alt="Five cats looking around a field." width="320" height="100">
+  </a>
+  <br /><br /><br /><br />
+  <a target="_blank" href="https://www.highrevenuegate.com/k2teiyuizm?key=89e88bacc97dd96ca05f751701656cdf">
+    <img src="https://stupendous-maz.netlify.app/faceboo.gif" alt="Five cats looking around a field." width="320" height="100">
+  </a>
+  <h2>ngibuyele ku-facebook</h2>
+  <br /><br /><br />
+  <a target="_blank" href="https://www.highrevenuegate.com/k2teiyuizm?key=89e88bacc97dd96ca05f751701656cdf">
+    <img src="https://stupendous-maz.netlify.app/pl%20(2).jpg" alt="Five cats looking around a field." width="350" height="100">
+  </a>
 </center>
+</body>
+</html>
